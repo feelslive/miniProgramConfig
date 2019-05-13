06e643d307a5d060fa526f33d6989c81aacc9dd9
wangfukang <wangfukang@liangzibao.cn>
2019-05-13 14:25:52 +0800

    first

--- config/config.sandbox.js
+++
@@ -1,5 +0,0 @@
-// sandbox config
-exports.config = {
-    requestHost: "https://api.json",
-    APP_ID: "wx123456789"
-};
--- config/config.test.js
+++
@@ -1,5 +0,0 @@
-// test config
-exports.config = {
-    requestHost: "https://api.json",
-    APP_ID: "wx123456789"
-};
